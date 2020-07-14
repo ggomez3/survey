1bc3ce00574bde48af9207521cc81b17d96e57c6
ggomez3 <67597052+ggomez3@users.noreply.github.com>
2020-07-14 13:23:01 -0500

    Add files via upload

--- jquery.js
+++ jquery.js
@@ -1,37 +1,99 @@
-import jQuery from "./core.js";
+// Use the right jQuery source on the test page (and iframes)
+( function() {
+	/* global loadTests: false */
 
-import "./selector.js";
-import "./traversing.js";
-import "./callbacks.js";
-import "./deferred.js";
-import "./deferred/exceptionHook.js";
-import "./core/ready.js";
-import "./data.js";
-import "./queue.js";
-import "./queue/delay.js";
-import "./attributes.js";
-import "./event.js";
-import "./event/trigger.js";
-import "./manipulation.js";
-import "./manipulation/_evalUrl.js";
-import "./wrap.js";
-import "./css.js";
-import "./css/hiddenVisibleSelectors.js";
-import "./css/showHide.js";
-import "./serialize.js";
-import "./ajax.js";
-import "./ajax/xhr.js";
-import "./ajax/script.js";
-import "./ajax/jsonp.js";
-import "./ajax/load.js";
-import "./core/parseXML.js";
-import "./core/parseHTML.js";
-import "./effects.js";
-import "./effects/animatedSelector.js";
-import "./offset.js";
-import "./dimensions.js";
-import "./deprecated.js";
-import "./exports/amd.js";
-import "./exports/global.js";
+	var dynamicImportSource, config, src,
+		FILEPATH = "/test/jquery.js",
+		activeScript = [].slice.call( document.getElementsByTagName( "script" ), -1 )[ 0 ],
+		parentUrl = activeScript && activeScript.src ?
+			activeScript.src.replace( /[?#].*/, "" ) + FILEPATH.replace( /[^/]+/g, ".." ) + "/" :
+			"../",
+		QUnit = window.QUnit,
+		require = window.require;
 
-export default jQuery;
+	function getQUnitConfig() {
+		var config = Object.create( null );
+
+		// Default to unminified jQuery for directly-opened iframes
+		if ( !QUnit ) {
+			config.dev = true;
+		} else {
+
+			// QUnit.config is populated from QUnit.urlParams but only at the beginning
+			// of the test run. We need to read both.
+			QUnit.config.urlConfig.forEach( function( entry ) {
+				config[ entry.id ] = QUnit.config[ entry.id ] != null ?
+					QUnit.config[ entry.id ] :
+					QUnit.urlParams[ entry.id ];
+			} );
+		}
+
+		return config;
+	}
+
+	// Define configuration parameters controlling how jQuery is loaded
+	if ( QUnit ) {
+		QUnit.config.urlConfig.push( {
+			id: "esmodules",
+			label: "Load as modules",
+			tooltip: "Load the jQuery module file (and its dependencies)"
+		}, {
+			id: "amd",
+			label: "Load with AMD",
+			tooltip: "Load the AMD jQuery file (and its dependencies)"
+		}, {
+			id: "dev",
+			label: "Load unminified",
+			tooltip: "Load the development (unminified) jQuery file"
+		} );
+	}
+
+	config = getQUnitConfig();
+
+	src = config.dev ?
+		"dist/jquery.js" :
+		"dist/jquery.min.js";
+
+	// Honor ES modules loading on the main window (detected by seeing QUnit on it).
+	// This doesn't apply to iframes because they synchronously expect jQuery to be there.
+	if ( config.esmodules && QUnit ) {
+
+		// Support: IE 11+, Edge 12 - 18+
+		// IE/Edge don't support the dynamic import syntax so they'd crash
+		// with a SyntaxError here.
+		dynamicImportSource = "" +
+			"import( `${ parentUrl }src/jquery.js` )\n" +
+			"	.then( ( { default: jQuery } ) => {\n" +
+			"		window.jQuery = jQuery;\n" +
+			"		if ( typeof loadTests === \"function\" ) {\n" +
+			"			// Include tests if specified\n" +
+			"			loadTests();\n" +
+			"		}\n" +
+			"	} )\n" +
+			"	.catch( error => {\n" +
+			"		console.error( error );\n" +
+			"		QUnit.done();\n" +
+			"	} );";
+
+		eval( dynamicImportSource );
+
+	// Apply similar treatment for AMD modules
+	} else if ( config.amd && QUnit ) {
+		require.config( {
+			baseUrl: parentUrl
+		} );
+		src = "amd/jquery";
+
+		// Include tests if specified
+		if ( typeof loadTests !== "undefined" ) {
+			require( [ src ], loadTests );
+		} else {
+			require( [ src ] );
+		}
+
+	// Otherwise, load synchronously
+	} else {
+		document.write( "<script id='jquery-js' nonce='jquery+hardcoded+nonce' src='" + parentUrl + src + "'><\x2Fscript>" );
+	}
+
+} )();
